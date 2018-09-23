6fefdfcc9e9d9109453b3477940619c0251f183f
Matthew Carter <mjcarter95@gmail.com>
2018-09-23 16:22:23 +0100

    Pythagorean triplet problem

---
+++ Project Euler/C/p009.c
@@ -0,0 +1,26 @@
+/*
+ * Solution to Problem 9 on Project Euler
+ * https://projecteuler.net/problem=9
+ */
+
+#include <stdio.h>
+
+int main()
+{
+
+  const int limit = 1000;
+  int a, b, c;
+
+  for(a = 1; a < limit; a++) {
+    for(b = a + 1; b < limit; b++) {
+      c = limit - a - b;
+
+      if(((a * a) + (b * b)) == (c * c)) {
+        printf("========\nSolution\n========\n");
+        printf("a: %d\nb: %d\nc: %d\n", a, b, c);
+      }
+    }
+  }
+
+  return 0;
+}
